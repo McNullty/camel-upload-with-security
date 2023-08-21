f5ef576795486de52a7f090354f0b7fbacc43fd8
Mladen Čikara <mladen.cikara@gmail.com>
2023-08-21 15:10:09 +0200

    Initial Import

---
+++ src/test/java/com/example/cameluploadwithsecurity/CamelUploadWithSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cameluploadwithsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelUploadWithSecurityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
